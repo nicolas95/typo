77b8d2d35ca8d28a5c840b1367614b40601894fb
nicolas.scherer <nicolas.scherer@gateprotect.com>
2016-01-22 10:41:26 +0100

    completed admin/categories_controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -13,6 +13,25 @@ class Admin::CategoriesController < Admin::BaseController
     end
   end
 
+  def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes(params)
+      rediret_to root_path
+    else
+      render 'edit'
+    end
+  end # update
+
+  def create
+    @category = Category.new(params)
+    if @category.save
+      redirect_to root_path
+    else
+      render 'new'
+    end # if
+
+  end # create
+
   def destroy
     @record = Category.find(params[:id])
     return(render 'admin/shared/destroy') unless request.post?
